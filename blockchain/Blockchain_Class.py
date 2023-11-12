9f110b272fd18b89a51517c750a929b70f104324
Faisal Banjar Windows Laptop <FaisalBj1@outlook.com>
2023-11-13 00:13:42 +0300

    separating the clas from the code - org

---
+++ blockchain/Blockchain_Class.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify, render_template
+from time import time
+from flask_cors import CORS
+from collections import OrderedDict
+import binascii
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+from uuid import uuid4
+import json
+import hashlib
+
+MINING_SENDER = "The Blockchain"
+MINING_REWARD = 1
+
+class Blockchain:
+
+    def __init__(self):
+        self.transactions = []
+        self.chain = []
+        self.node_id = str(uuid4()).replace('-', '')
+        # Create the genesis block
+        self.create_block(0, '00')
+
+    def create_block(self, nonce, previous_hash):
+        """
+        Add a block of transactions to the blockchain
+        """
+        block = {'block_number': len(self.chain) + 1,
+                 'timestamp': time(),
+                 'transactions': self.transactions,
+                 'nonce': nonce,
+                 'previous_hash': previous_hash}
+
+        # Reset the current list of transactions
+        self.transactions = []
+        self.chain.append(block)
+        return block
+
+    def verify_transaction_signature(self, sender_public_key, signature, transaction):
+        public_key = RSA.importKey(binascii.unhexlify(sender_public_key))
+        verifier = PKCS1_v1_5.new(public_key)
+        h = SHA.new(str(transaction).encode('utf8'))
+        try:
+            verifier.verify(h, binascii.unhexlify(signature))
+            return True
+        except ValueError:
+            return False
+
+    def proof_of_work(self):
+        return 12345
+
+    def hash(self, block):
+        # We must ensure that the Dictionary is ordered, otherwise we'll get inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode('utf8')
+        h = hashlib.new('sha256')
+        h.update(block_string)
+        return h.hexdigest()
+
+    def submit_transaction(self, sender_public_key, recipient_public_key, signature, amount):
+        transaction = OrderedDict({
+            'sender_public_key': sender_public_key,
+            'recipient_public_key': recipient_public_key,
+            'amount': amount
+        })
+
+        # Reward for mining a block
+        if sender_public_key == MINING_SENDER:
+            self.transactions.append(transaction)
+            return len(self.chain) + 1
+        else:
+            # Transaction from wallet to another wallet
+            signature_verification = self.verify_transaction_signature(sender_public_key, signature, transaction)
+            if signature_verification:
+                self.transactions.append(transaction)
+                return len(self.chain) + 1
+            else:
+                return False
+
