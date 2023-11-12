b5f915de012f3aa765b908b29b5bd830f2700f27
Faisal Banjar Windows Laptop <FaisalBj1@outlook.com>
2023-11-13 00:22:53 +0300

    org

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -3,31 +3,9 @@ import Crypto
 import Crypto.Random
 from Crypto.PublicKey import RSA
 import binascii
-from collections import OrderedDict
-from Crypto.Signature import PKCS1_v1_5
-from Crypto.Hash import SHA
+# my own
+from Transaction import Transaction
 
-
-class Transaction:
-
-    def __init__(self, sender_public_key, sender_private_key, recipient_public_key, amount):
-        self.sender_public_key = sender_public_key
-        self.sender_private_key = sender_private_key
-        self.recipient_public_key = recipient_public_key
-        self.amount = amount
-
-    def to_dict(self):
-        return OrderedDict({
-            'sender_public_key': self.sender_public_key,
-            'recipient_public_key': self.recipient_public_key,
-            'amount': self.amount,
-        })
-
-    def sign_transaction(self):
-        private_key = RSA.importKey(binascii.unhexlify(self.sender_private_key))
-        signer = PKCS1_v1_5.new(private_key)
-        h = SHA.new(str(self.to_dict()).encode('utf8'))
-        return binascii.hexlify(signer.sign(h)).decode('ascii')
 
 
 app = Flask(__name__)
