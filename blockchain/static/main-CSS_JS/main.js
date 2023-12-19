dd6c474004728ef7493b25194771b17eb3016875
Faisal Banjar Windows Laptop <FaisalBj1@outlook.com>
2023-12-19 18:30:23 +0300

    all good

--- blockchain/static/main-CSS_JS/main.js
+++ blockchain/static/main-CSS_JS/main.js
@@ -23,51 +23,58 @@ window.addEventListener('load', () => {
 // -------------------------------------------------------------------------------------------------------------------------------------
 // theme control---
 
-var theme = root.getAttribute('data-theme');
-
-// initializing/retrieving the theme
-// if ((localStorage.getItem("theme") != 'dark') && (localStorage.getItem("theme") != 'light')){
-if (localStorage.getItem("theme") == null){
-    root.setAttribute('data-theme', 'dark');
-} else {
-    root.setAttribute('data-theme', localStorage.getItem("theme"));
-}
-
-//storing the theme
-const storeTheme = function(theme){
-    localStorage.setItem("theme", theme);
-}
 window.addEventListener('load', () => {
-    // Initialize the toggle icon based on the current theme
-    updateToggleIcon();
+    const colorThemes = document.querySelectorAll('[name="theme"]');
 
-    // Theme toggle
-    document.getElementById('theme_icon').addEventListener('click', function () {
-        // Toggle the theme
-        if (theme === 'light') {
-            root.setAttribute('data-theme', 'dark');
-        } else {
-            root.setAttribute('data-theme', 'light');
+    // set theme when visitor returns
+    const setTheme = function () {
+        const activeTheme = localStorage.getItem("theme");
+        colorThemes.forEach((themeOption) => {
+            if (themeOption.id === activeTheme) {
+                themeOption.checked = true;
+            }
+        });
+        root.setAttribute('data-theme', activeTheme);
+    };
+
+
+    // store theme
+    const storeTheme = function (theme) {
+        localStorage.setItem("theme", theme);
+    };
+
+
+    // Initializing the theme
+    function isThemeInLocalStorage() {
+        return localStorage.getItem('theme') !== null;
         }
-        theme = root.getAttribute('data-theme');
 
-        storeTheme(theme);
+    if (isThemeInLocalStorage()) {
+        // 'theme' exists in local storage
+        console.log('Theme exists:', localStorage.getItem('theme'));
+    } else {
+        // 'theme' does not exist in local storage
+        console.log('Theme does not exist in local storage');
+        root.setAttribute('data-theme', 'darkness');
+        localStorage.setItem("theme", 'darkness');
 
-        // Update the toggle icon
-        updateToggleIcon();
-    });
-
-    function updateToggleIcon() {
-        // Update the toggle icon based on the current theme
-        if (theme === 'dark') {
-            document.getElementById('theme_icon').classList.add('fa-sun');
-            document.getElementById('theme_icon').classList.remove('fa-moon');
-        } else {
-            document.getElementById('theme_icon').classList.remove('fa-sun');
-            document.getElementById('theme_icon').classList.add('fa-moon');
-        }
+        const activeTheme = localStorage.getItem("theme");
+        colorThemes.forEach((themeOption) => {
+            if (themeOption.id === activeTheme) {
+                themeOption.checked = true;
+            }
+        });
+        root.setAttribute('data-theme', activeTheme);
     }
 
+    colorThemes.forEach((themeOption) => {
+        themeOption.addEventListener("click", () => {
+            storeTheme(themeOption.id);
+            root.setAttribute('data-theme', themeOption.id);
+        });
+    });
+
+    document.onload = setTheme();
 });
 
 // -------------------------------------------------------------------------------------------------------------------------------------
@@ -76,91 +83,188 @@ window.addEventListener('load', () => {
 class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
+            <!-- header / nav  -->
             <style>
                 my-header {
-                    width: 100%;
+                    position: relative;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    z-index: 9999999;
+                    min-width: 100vw;
                     margin-bottom: auto;
                     backdrop-filter: blur(15px);
                     box-shadow: 0px 1px 31px 1px var(--shadow-color);
                 }
-                my-header * {
+                header {
+                    display: flex;
+                    flex-direction: column;
+                    min-width: 100%;
+                }
+
+                header * {
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
                     color: var(--primary-color);
+                }
+
+                nav {
+                    box-sizing: border-box;
+                    border-bottom: solid 1px var(--primary-color);
+                    min-width: 100%;
+                    display: flex;
+                    justify-content: space-between;
+                    margin: 0;
+                    padding: 9px 15px;
+                }
+
+                nav .logo {
+                    order: 1;
+                    font-weight: 900;
+                }
+
+                nav .logo a {
                     text-decoration: none;
                 }
-                nav {
-                    display: grid;
-                    grid-template-areas: 'logo nav_menu icons';
-                    padding: 0 1.5rem;
-                    background: transparent;
-                    margin: 0;
-                    justify-content: stretch;
-                }
-                nav .logo {
-                    grid-area: logo;
-                    display: flex;
-                    flex-direction: row;
-                    justify-content: start;
-                    align-items: center;
 
-                }
                 nav .icons {
-                    grid-area: icons;
+                    order: 3;
                     display: flex;
-                    flex-direction: row;
-                    justify-content: end;
+                    justify-content: center;
                     align-items: center;
                     gap: 15px;
                 }
-                nav .menu {
-                    grid-area: nav_menu;
-                    display: flex;
-                    flex-direction: row;
+
+                nav #menu_toggle {
+                    cursor: pointer;
+                    display: none;
                 }
-                nav .menu ul{
-                    width: 100%;
-                    display: flex;
-                    flex-direction: row;
-                    gap: 21px;
-                    transform: translate(-4rem);
-                    place-content: center;
+
+                nav #theme_toggle {
+                    cursor: pointer;
                 }
-                nav .menu * {
+
+                .menu {
+                    order: 2;
+                }
+
+                .menu *{
                     text-decoration: none;
                     list-style: none;
+                    font-weight: 500;
                 }
-                nav i {
-                    font-size: 1.35rem;
+
+                .menu ul {
+                    display: flex;
+                    gap: 15px;
+                }
+
+                #menu-1 {
+                    transform: translate(-3rem);
+                }
+
+                #menu-2 {
+                    display: none;
+                    justify-content: center;
+                    padding: 15px 5px;
+                    border-bottom: solid 1px var(--primary-color);
+                    animation: fade_in 250ms ease-in-out;
+                }
+                #menu-2 ul {
+                    flex-direction: column;
+                }
+
+                .themes {
+                    order: 5;
+                    padding: 10px 0;
+                    border-bottom: solid 1px var(--primary-color);
+                    display: none;
+                    justify-content: center;
+                    animation: fade_in 250ms ease-in-out;
+                }
+
+                /* style for different screens */
+                /* [laptop] */
+                @media screen and (min-width: 1025px){
+
+                }
+                /* [ipad] */
+                @media screen and (min-width: 768px) and (max-width: 1024px) {
+                    nav #menu_toggle {
+                        display: block;
+                    }
+                    #menu-1 {
+                        display: none;
+                    }
+                }
+                /* [phone] */
+                @media screen and (max-width: 767px){
+                    nav #menu_toggle {
+                        display: block;
+                    }
+                    #menu-1 {
+                        display: none;
+                    }
+                }
+            </style>
+
+            <!-- themes options -->
+            <style>
+                .visually-hidden {
+                    clip: rect(0 0 0 0);
+                    clip-path: inset(50%);
+                    height: 1px;
+                    overflow: hidden;
+                    position: absolute;
+                    white-space: nowrap;
+                    width: 1px;
+                }
+
+                .color-picker > fieldset {
+                    border: 0;
+                    display: flex;
+                    gap: 1rem;
+                    width: fit-content;
+                    padding: 0;
+                    margin: 0;
+                    border-radius: 0 0 1rem 1rem;
+                }
+
+                .color-picker input[type="radio"] {
+                    appearance: none;
+                    width: .5rem;
+                    height: .5rem;
+                    aspect-ratio: 1/1;
+                    outline: 2px solid var(--radio-color, currentColor);
+                    outline-offset: 1.5px;
+                    border-radius: 50%;
                     cursor: pointer;
                 }
 
-                nav ul li:hover, nav ul a:hover, #theme_icon:hover {
-                    transition: all 0.15s ease-out;
-                    transform: scale(1.05);
-                    filter: drop-shadow(0px 0px 1px var(--accent-color));
-                    filter: drop-shadow(0px 0px 3px var(--accent-color));
-                    filter: drop-shadow(0px 0px 5px var(--accent-color));
-                    filter: drop-shadow(0px 0px 7px var(--accent-color));
-                    filter: drop-shadow(0px 0px 9px var(--accent-color));
-                    filter: drop-shadow(0px 0px 11px var(--accent-color));
-                    filter: drop-shadow(0px 0px 13px var(--accent-color));
-                    filter: drop-shadow(0px 0px 15px var(--accent-color));
-                }
-                nav ul li:not(hover) {
-                    transition: all 0.25s ease-out;
+                .color-picker input[type="radio"]:checked {
+                    background-color: var(--radio-color);
                 }
 
-                .menu_icon_anim {
-                    display: none;
-                    cursor: pointer;
+                .color-picker input[type="radio"]#light {
+                    --radio-color: rgb(188, 188, 255);
                 }
-                .menu_icon_anim input {
-                    display: none;
+                .color-picker input[type="radio"]#dark {
+                    --radio-color: rgb(81, 81, 172);
                 }
-                .menu_icon_anim svg {
+                .color-picker input[type="radio"]#darkness {
+                    --radio-color: rgb(0, 0, 0);
+                }
+            </style>
+
+            <!-- menu toggle icon -->
+            <style>
+                .menu_icon_animation svg {
                     /* The size of the SVG defines the overall size */
-                    height: 2rem;
+                    height: 1.5rem;
                     /* Define the transition for transforming the SVG */
                     transition: transform 750ms cubic-bezier(0.4, 0, 0.2, 1);
+                    cursor: pointer;
                 }
                 .line {
                     fill: none;
@@ -177,89 +281,87 @@ class MyHeader extends HTMLElement {
                 .line-top-bottom {
                     stroke-dasharray: 12 63;
                 }
-                .menu_icon_anim input:checked + svg {
+                .menu_icon_animation input:checked + svg {
                     transform: rotate(-45deg);
                 }
-                .menu_icon_anim input:checked + svg .line-top-bottom {
+                .menu_icon_animation input:checked + svg .line-top-bottom {
                     stroke-dasharray: 20 300;
                     stroke-dashoffset: -32.42;
                 }
-
-                /* style for smaller screens */
-                @media (max-width: 50rem){
-                    .menu_icon_anim {
-                        display: block;
-                    }
-                    nav {
-                        grid-template-areas:
-                        'logo icons'
-                        'nav_menu nav_menu';
-                        padding: 0.5rem 1.5rem;
-                    }
-                    nav .menu {
-                        display: none;
-                    }
-                    nav .menu ul{
-                        flex-direction: column;
-                        transform: translate(-1.5rem);
-                    }
-                    nav #toggle-02:checked ~ .menu {
-                        display: block;
-                        animation: menu_slide 0.75s ease-out;
-                    }
-                }
-
-                @keyframes menu_slide {
-                    from {
-                        opacity: 0;
-                        transform: translateY(-17px);
-                    } to {
-                        opacity: 1;
-                        transform: translateY(0);
-                    }
-                }
             </style>
 
-            <nav>
-                <a class="logo" href="/" style="font-weight: bold;">block drive - Blockchain based app</a>
 
-                <input type="checkbox" id="toggle-02" style="display: none;">
-                <div class="icons">
-                    <label class="menu_icon_anim" id="menu_icon" for="toggle">
-                        <input type="checkbox" id="toggle" style="display: none;">
-                        <svg viewBox="0 0 32 32">
-                        <path class="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
-                        <path class="line" d="M7 16 27 16"></path>
-                        </svg>
-                    </label>
-                    <div>
-                        <label><i id="theme_icon" class="fa-solid fa-sun"></i></label>
+            <header>
+                <nav>
+                    <div class="logo"><a href="/">Blockchain Demo - Miner</a></div>
+
+                    <div class="icons">
+                        <label class="menu_icon_animation" for="toggle">
+                            <input type="checkbox" id="toggle" style="display: none;">
+                            <svg id="menu_toggle" viewBox="0 0 32 32">
+                            <path class="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+                            <path class="line" d="M7 16 27 16"></path>
+                            </svg>
+                        </label>
+                        <i id="theme_toggle" class="fa-solid fa-circle-half-stroke"></i>
                     </div>
-                </div>
 
-                <div class="menu">
+                <div id="menu-1" class="menu">
                     <ul>
                         <li><a href="/mine_page">MINE</a></li>
                         <li><a href="/configure">CONFIGURE</a></li>
                     </ul>
                 </div>
             </nav>
+
+                <div id="menu-2" class="menu">
+                    <ul>
+                        <li><a href="/mine_page">MINE</a></li>
+                        <li><a href="/configure">CONFIGURE</a></li>
+                    </ul>
+                </div>
+
+                <div id="themes" class="themes">
+                    <form class="color-picker" action="">
+                        <fieldset>
+                            <legend class="visually-hidden">Pick a color scheme</legend>
+                            <label for="light" class="visually-hidden">light theme</label>
+                            <input type="radio" id="light" name="theme">
+
+                            <label for="dark" class="visually-hidden">dark theme</label>
+                            <input type="radio" id="dark" name="theme">
+
+                            <label for="darkness" class="visually-hidden">darkness theme</label>
+                            <input type="radio" id="darkness" name="theme">
+                        </fieldset>
+                    </form>
+                </div>
+            </header>
             `
     }
 }
 
 customElements.define('my-header', MyHeader)
 
+// themes & nav menu toggles
 window.addEventListener('load', () => {
-    document.getElementById('menu_icon').addEventListener('click', function() {
-        if (document.getElementById('toggle').checked) {
-            document.getElementById('toggle-02').checked = true;
+    document.getElementById('theme_toggle').addEventListener('click', function () {
+        if (document.getElementById('themes').style.display == 'flex'){
+            document.getElementById('themes').style.display = 'none';
         } else {
-            document.getElementById('toggle-02').checked = false;
+            document.getElementById('themes').style.display = 'flex';
         }
     });
-});
 
+    document.getElementById('menu_toggle').addEventListener('click', function () {
+        if (document.getElementById('menu-2').style.display == 'flex'){
+            document.getElementById('menu-2').style.display = 'none';
+        } else {
+            document.getElementById('menu-2').style.display = 'flex';
+        }
+    });
+
+});
 // -------------------------------------------------------------------------------------------------------------------------------------
 // footer code---
 
@@ -268,24 +370,29 @@ class MyFooter extends HTMLElement {
         this.innerHTML = `
             <style>
                 my-footer{
-                    /*position: absolute;
-                    bottom:0;
-                    z-index: 9999999;*/
-                    box-sizing: border-box;
-                    width: 100%;
+                    position: relative;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                    z-index: 9999999;
+                    min-width: 100vw;
                     margin-top: auto;
                     background: transparent;
                     backdrop-filter: blur(15px);
                     box-shadow: 0px 1px 31px 1px var(--shadow-color);
                 }
                 footer {
+                    min-width: 100%;
+                    box-sizing: border-box;
+                    border-top: solid 1px var(--primary-color);
+                    backdrop-filter: blur(15px);
                     display: flex;
                     flex-direction: column;
                     justify-content: space-between;
                     text-align: center;
                     align-items: center;
-                    gap: 1.5rem;
-                    padding: 25px 25px 11px 25px;
+                    gap: 1rem;
+                    padding: 9px 15px;
                 }
                     footer a {
                         color: var(--accent-color);
